2476801f21a1a2cf76b839562c431446ec735c57
anushree410 <anudeota@gmail.com>
2025-08-31 00:53:11 +0530

    showdb in logs

--- django_project/api/management/commands/showdb.py
+++ django_project/api/management/commands/showdb.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
+import os
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        self.stdout.write("DBURL:"+str(os.getenv('DATABASE_URL')))
         self.stdout.write(str(connection.settings_dict))
